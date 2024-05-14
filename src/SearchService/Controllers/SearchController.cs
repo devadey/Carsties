98b8c6a9ae3032240318a92ee4b4056ea6faaf60
Adey Sam <dev.codermail@gmail.com>
2024-05-14 09:40:08 -0700

    RequestHelpers introduced and endpoint is configured to read the search parameters from query. FilterBy, OrderBy, Winner, Seller, SearchTerm implementation reconfigured.

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers
 {
@@ -14,25 +15,50 @@ namespace SearchService.Controllers
     {
 
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber=1, int pageSize=4)
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
         {
-            var query = DB.PagedSearch<Item>();
+            var query = DB.PagedSearch<Item, Item>();
 
-            query.Sort(b => b.Ascending(s => s.Make));
+            ;
 
-            if (!string.IsNullOrEmpty(searchTerm))
+            if (!string.IsNullOrEmpty(searchParams.searchTerm))
             {
-                query.Match(Search.Full, searchTerm).SortByTextScore();
+                query.Match(Search.Full, searchParams.searchTerm).SortByTextScore();
             }
 
-            query.PageNumber(pageNumber);
-            query.PageSize(pageSize);
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(b => b.Ascending(s => s.Make)),
+                "new" => query.Sort(b => b.Descending(s => s.CreatedAt)),
+                _ => query.Sort(b => b.Ascending(s => s.AuctionEnd))
+            };
+
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(b => b.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(b => b.AuctionEnd < DateTime.UtcNow.AddHours(6) && b.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(b => b.AuctionEnd > DateTime.UtcNow)
+            };
+
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+            {
+                query.Match(x => x.Seller == searchParams.Seller);
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.Winner))
+            {
+                query.Match(x => x.Winner == searchParams.Winner);
+            }
+
+            query.PageNumber(searchParams.pageNumber);
+            query.PageSize(searchParams.PageSize);
 
             var result = await query.ExecuteAsync();
             if (result.TotalCount == 0)
                 return NotFound("No result matches the search term.");
-            return Ok(new{
-                result = result.Results,
+            return Ok(new
+            {
+                results = result.Results,
                 pageCount = result.PageCount,
                 totalCount = result.TotalCount
             });
